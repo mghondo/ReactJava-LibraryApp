975df08d6d4e44298ba07be44badb16cc749e53f
Morgan Hondros <morganhondros@gmail.com>
2024-07-01 09:02:30 -0400

    Right before incorporating Springboot.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/components/Carousel';
+import { ReturnBook } from './layouts/HomePage/components/ReturnBook';
+import { Heros } from './layouts/HomePage/components/Heroes';
+import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
-function App() {
+export const App = () => {
   return (
       <div>
-      <Navbar/>
+        <HomePage/>
 
-      <ExploreTopBooks/>
       </div>
   );
 }
 
-export default App;
